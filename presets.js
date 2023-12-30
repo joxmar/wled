e1c4f6ba2ecbdbc1a8a50e5ee98e30a00f468265
joxmar <jorge.elvir@gmail.com>
2023-12-30 00:50:07 -0600

    Update presets.js
    added Astros colors

--- presets.js
+++ presets.js
@@ -34,3 +34,7 @@
 
 // red green and white running around in dark
 {"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[8,255,0],[255,0,0],[255,255,255]],"fx":112,"sx":128,"ix":128,"pal":5,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+
+/******* ASTROS SUPPORTS ******/
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[8,255,0],[255,0,0],[255,255,255]],"fx":110,"sx":128,"ix":69,"pal":13,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
