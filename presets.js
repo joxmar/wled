e87a9f59bed409e66b0d456468c5b49864c5c46b
joxmar <jorge.elvir@gmail.com>
2024-01-08 19:02:26 -0600

    Update presets.js
    new years presets

--- presets.js
+++ presets.js
@@ -38,3 +38,34 @@
 
 /******* ASTROS SUPPORTS ******/
 {"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[8,255,0],[255,0,0],[255,255,255]],"fx":110,"sx":128,"ix":69,"pal":13,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+
+
+/******* new years ******/
+//blue swipe with sparks
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,211,171],[0,0,0],[0,0,0]],"fx":87,"sx":128,"ix":128,"pal":60,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+//yeallow and red with sparks
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,211,171],[0,0,0],[0,0,0]],"fx":87,"sx":38,"ix":163,"pal":69,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+// rainbow grow
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,211,171],[0,0,0],[0,0,0]],"fx":137,"sx":128,"ix":128,"pal":13,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":2},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+// fireworks
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,211,171],[0,0,0],[0,0,0]],"fx":89,"sx":128,"ix":128,"pal":11,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+//more fireworks
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,211,171],[0,0,0],[0,0,0]],"fx":90,"sx":128,"ix":128,"pal":67,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+// fireworks 2
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[0,0,0],[0,0,0],[0,0,0]],"fx":89,"sx":229,"ix":215,"pal":11,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+// fireworks 3
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[0,0,0],[0,0,0],[0,0,0]],"fx":89,"sx":128,"ix":128,"pal":39,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+// red gold twinkles
+{"on":true,"bri":220,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":482,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,0,0],[0,0,0],[255,200,0]],"fx":42,"sx":128,"ix":231,"pal":5,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}
+
+
+
+
